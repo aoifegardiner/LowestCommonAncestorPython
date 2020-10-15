13e8e1c7f8741e01fd6a9dabc9f18b4ca5708065
aoifegardiner <60934384+aoifegardiner@users.noreply.github.com>
2020-10-15 13:07:42 +0100

    Add files via upload

---
+++ Lowest Common Ancestor.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 13:40:28 2020
+
+@author: aoife
+"""
+
+class Node: 
+       
+    def __init__(self, key): 
+        self.key = key  
+        self.left = None
+        self.right = None
+      
+
+def findLCA(root, n1, n2): 
+      
+    if root is None: 
+        return None
+  
+    if root.key == n1 or root.key == n2: 
+        return root  
+  
+    left_lca = findLCA(root.left, n1, n2)  
+    right_lca = findLCA(root.right, n1, n2) 
+  
+    if left_lca and right_lca: 
+        return root  
+  
+    return left_lca if left_lca is not None else right_lca 
