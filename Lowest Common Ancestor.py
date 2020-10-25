fe1c08186c43fc45e1135da94e46414590e45055
aoifegardiner <60934384+aoifegardiner@users.noreply.github.com>
2020-10-25 13:08:14 +0000

    Update Lowest Common Ancestor.py

--- Lowest Common Ancestor.py
+++ Lowest Common Ancestor.py
@@ -28,3 +28,11 @@ def findLCA(root, n1, n2):
         return root  
   
     return left_lca if left_lca is not None else right_lca 
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
